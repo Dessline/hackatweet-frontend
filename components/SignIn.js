b915d4df8b99222adce48ad2618e8901324358fb
Dessline <desailles.dessline@outlook.fr>
2024-02-15 13:51:41 +0100

    added files

---
+++ components/SignIn.js
@@ -0,0 +1,15 @@
+import styles from '../styles/SingIn.module.css';
+
+function SingIn() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default SingIn;
---
+++ components/SignUp.js
@@ -0,0 +1,15 @@
+import styles from '../styles/SingUp.module.css';
+
+function SingUp() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default SingUp;
